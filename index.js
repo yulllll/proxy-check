2150ee61cbde9a8d306bc2d9c38975f9e70e05f9
Yulia Koretskaya <yulia.koretskaya@gmail.com>
2023-05-18 15:29:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 app.use('/', createProxyMiddleware({ target: 'http://51.250.72.142:1337', changeOrigin: true }));
-app.listen(3000);
+app.listen(8080);
