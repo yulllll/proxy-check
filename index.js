ba53be81511626b56308097ebc5a711eb573dcc8
yulllll <yulia.koretskaya@gmail.com>
2023-05-18 12:23:34 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use('/', createProxyMiddleware({ target: 'http://51.250.72.142:1337', changeOrigin: true }));
+app.listen(3000);
